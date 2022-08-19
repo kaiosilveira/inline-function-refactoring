2608e8127cf8a45b4b1407b5fef8e4b622ed234b
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-19 06:36:58 +0100

    rename moreThanFiveLateDeliveries parameter

--- rating.js
+++ rating.js
@@ -2,8 +2,8 @@ function getRating(driver) {
   return moreThanFiveLateDeliveries(driver) ? 2 : 1;
 }
 
-function moreThanFiveLateDeliveries(dvr) {
-  return dvr.numberOfLateDeliveries > 5;
+function moreThanFiveLateDeliveries(driver) {
+  return driver.numberOfLateDeliveries > 5;
 }
 
 module.exports = { getRating };
