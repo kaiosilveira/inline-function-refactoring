33dd6b6996abad72b4a88deb9863ecb1bfcf5fc1
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-19 06:38:59 +0100

    remove moreThanFiveLateDeliveries function

--- rating.js
+++ rating.js
@@ -2,8 +2,4 @@ function getRating(driver) {
   return driver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
 
-function moreThanFiveLateDeliveries(driver) {
-  return driver.numberOfLateDeliveries > 5;
-}
-
 module.exports = { getRating };
